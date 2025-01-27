96f33b0f4b080f218c958bd6c1f95ff008e1122a
Fabio Mangione <fmangione@salesforce.com>
2025-01-27 14:15:49 +0000

    small change

--- run_code_coverage.sh
+++ run_code_coverage.sh
@@ -1,11 +1,15 @@
-bazel test --cache_test_results=no --action_env=VERBOSE_COVERAGE=true --collect_code_coverage --combined_report=lcov --instrumentation_filter "^//projects/libs/examples/calculator[/:]" -- projects/libs/examples/calculator/...
+rm -f bazel-out/_coverage/_coverage_report.dat
+
+bazel coverage --cache_test_results=no --action_env=VERBOSE_COVERAGE=true --combined_report=lcov --instrumentation_filter "^//projects/libs/examples/calculator[/:]" -- projects/libs/examples/calculator/...
+
+ls bazel-out/darwin_arm64-fastbuild/testlogs/projects/libs/examples/calculator/calculator_test/coverage.dat
+
 
 echo "Checking the content of the report at bazel-out/_coverage/_coverage_report.dat"
-
 cat bazel-out/_coverage/_coverage_report.dat
 
 if [ -s "bazel-out/_coverage/_coverage_report.dat" ]; then
-    echo "File is not empty."
+    echo "Code coverage report is not empty."
     exit 0
 else
     echo "ERROR: Code coverage report is empty."
